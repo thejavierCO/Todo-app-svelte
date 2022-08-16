4ada8b4b59de74cf4e3680da990650cea89321d8
Javier Cruz <31808656+thejavierCO@users.noreply.github.com>
2022-08-16 00:06:28 +0000

    old data

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,64 +1,127 @@
 <script>
-  // import Main from "./Main.svelte";
-  // import Task from "./task.svelte";
+  import Main from "./Main.svelte";
+  import Task from "./task.svelte";
   export let app;
-  setInterval(() => {
-    app.add({ id: Math.random() * 100 });
-  }, 3000);
-  app.on("change", (e) => {
-    console.log(e);
-  });
-  console.log(app);
-  // let newTask = ({detail})=>app.item = detail;
-  // let edit = ({detail:a})=>{
-  // 	let {parent} = a;
-  // 	let controlId = document.querySelector("#newTask").querySelector(".idNewTask");
-  // 	let controlName = document.querySelector("#newTask").querySelector(".nameNewTask");
-  // 	let controlDescription = document.querySelector("#newTask").querySelector(".descriptionNewTask");
-  // 	let id = parent.getAttribute("task");
-  // 	let name = parent.querySelector(".name").textContent;
-  // 	let description = parent.querySelector(".description").textContent;
-  // 	id = parseInt(id);
-  // 	controlId.value = id;
-  // 	controlName.value = name;
-  // 	controlDescription.value = description
-  // }
-  // let delTask = ({detail:a})=>{
-  // 	let {parent} = a;
-  // 	let id = parent.getAttribute("task");
-  // 	id = parseInt(id);
-  // 	app.data = app.data.filter(e=>e.id!==id);
-  // }
-  // let update = (tag)=>{
-  // 	app.data.map(task=>new Task({
-  // 		target:tag,
-  // 		props:{
-  // 			type:"print",
-  // 			task
-  // 		}
-  // 	}))
-  // 	.map(e=>{
-  // 		e.$on("edit",edit)
-  // 		e.$on("delete",delTask)
-  // 	})
-  // 	app.update(e=>{
-  // 		tag.innerHTML = "";
-  // 		e.map(task =>new Task({
-  // 			target:tag,
-  // 			props:{
-  // 				type:"print",
-  // 				task
-  // 			}
-  // 		}))
-  // 		.map(e=>{
-  // 			e.$on("edit",edit)
-  // 			e.$on("delete",delTask)
-  // 		})
-  // 	})
-  // }
+  /*
+    let newTask = ({ detail: { id, name, description } }) => {
+      id = +id;
+      if (typeof id == "number") app.get(id).data = { name, description };
+      else app.add({ name, description });
+    };
+    let delTask = ({ detail: a }) => {
+      let { parent } = a;
+      let id = parent.getAttribute("task");
+      id = parseInt(id);
+      app.del(id);
+    };
+    let update = (tag) => {
+      app.data
+      .map(
+        (task) =>
+        new Task({
+          target: tag,
+          props: {
+            type: "print",
+            task: task.data,
+          },
+        })
+        )
+        .map((e) => {
+          e.$on("edit", edit);
+          e.$on("delete", delTask);
+        });
+        app.on("change", ({ detail: { add: e } }) => {
+          if (e) {
+            tag.innerHTML = "";
+            e.map(
+              (task) =>
+              new Task({
+                target: tag,
+                props: {
+                  type: "print",
+                  task: task.data,
+                },
+              })
+          ).map((e) => {
+            e.$on("edit", edit);
+            e.$on("delete", delTask);
+          });
+        }
+      });
+    };
+  */
+  const edit = ({ detail: a }) => {
+    let { parent } = a;
+    let controlId = document
+      .querySelector("#newTask")
+      .querySelector(".idNewTask");
+    let controlName = document
+      .querySelector("#newTask")
+      .querySelector(".nameNewTask");
+    let controlDescription = document
+      .querySelector("#newTask")
+      .querySelector(".descriptionNewTask");
+    let id = parent.getAttribute("task");
+    let name = parent.querySelector(".name").textContent;
+    let description = parent.querySelector(".description").textContent;
+    id = parseInt(id);
+    controlId.value = id;
+    controlName.value = name;
+    controlDescription.value = description;
+  };
+  const newTask = ({ detail: { id, name, description } }) => {
+    let task = app.get(id);
+    if (task) {
+      task.data = { id, name, description };
+    } else app.add({ id, name, description });
+  };
+
+  const delTask = ({ detail: a }) => {
+    let { parent } = a;
+    let id = parent.getAttribute("task");
+    id = parseInt(id);
+    app.del(id);
+  };
+  const update = (tag) => {
+    app.data
+      .map(
+        (task) =>
+          new Task({
+            target: tag,
+            props: {
+              type: "print",
+              task: task.data,
+            },
+          })
+      )
+      .map((e) => {
+        e.$on("edit", edit);
+        e.$on("delete", delTask);
+      });
+    app.on("change", ({ detail: { add, del } }) => {
+      if (add || del) {
+        tag.innerHTML = "";
+        (add || del)
+          .map(
+            (task) =>
+              new Task({
+                target: tag,
+                props: {
+                  type: "print",
+                  task: task.data,
+                },
+              })
+          )
+          .map((e) => {
+            e.$on("edit", edit);
+            e.$on("delete", delTask);
+          });
+      }
+    });
+  };
 </script>
 
-<!-- <Main>
+<Main>
   <div slot="navbar">
     <div class="container">
       <h3>Todo app</h3>
@@ -70,4 +133,4 @@
     </div>
     <div class="row row-cols-4" use:update />
   </div>
-</Main> -->
+</Main>
