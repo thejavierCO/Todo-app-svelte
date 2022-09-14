e2cb6e01ebd60c19b29eff38082294c21c3d5d4c
Javier Cruz <31808656+thejavierCO@users.noreply.github.com>
2022-09-14 15:49:52 +0000

    add item component

--- src/components/print.svelte
+++ src/components/print.svelte
@@ -1,12 +1,13 @@
 <script>
+  // import { onMount, onDestroy } from "svelte";
+  import Item from "./item.svelte";
   export let app;
 </script>
 
 <div class="row row-cols-4">
   {#each app.data as { data: item }}
     <div class="print">
-      <span class="task_title">{item.data.name}</span>
-      <span class="task_description">{item.data.description}</span>
+      <Item title={item.data.name} description={item.data.description} />
     </div>
   {/each}
 </div>
@@ -22,18 +23,5 @@
     background-color: rgba(0, 0, 0, 0.5);
     color: #fff;
     max-height: 300px;
-  }
-  div.print span {
-    padding: 5px;
-    display: flex;
-    word-break: break-word;
-    width: 100%;
-    height: 100%;
-  }
-  div.print span.task_title {
-    background-color: rgba(0, 0, 0, 0.2);
-  }
-  div.print span.task_description {
-    background-color: rgba(0, 0, 0, 0.3);
   }
 </style>
