f39c6e501da955f69b53f6ac905794df5a353e87
portalcruz117 <portalcruz117@gmail.com>
2024-06-02 23:58:41 +0000

    add checkbox

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -15,13 +15,20 @@
       <form slot="input" on:submit|preventDefault={({target:a})=>{
           let data = {};
           let inputs = a.querySelectorAll("input[name]");
-          inputs.forEach(({name,value})=>value!=""?data[name] = value:"");
+          inputs.forEach((a)=>{
+            if(a.type == "checkbox"){
+              data[a.name] = a.checked;
+            }else a.value!=""?data[a.name] = a.value:""
+          });
           if(isEdit) edit(data.id,data);else add(data);
-          inputs.forEach((a)=>a.value = "");
+          inputs.forEach((a)=>{
+            if(a.type == "checkbox") a.checked = false;else a.value = ""
+          });
           if(isEdit!=false)isEdit = false;
         }} class="justify-content-center render row">
         <input type="text" placeholder="title" name="title"/>
         <input type="text" placeholder="description" name="description"/>
+        <input type="checkbox" name="done">
         <input type="text" name="id" disabled/>
         {#if isEdit}
           <input type="submit" value="edit">
@@ -29,15 +36,15 @@
           <input type="submit" value="add">
         {/if}
       </form>
-      <div slot="print" let:id let:data let:index class="card bg-secondary text-white">
-        <div class="card-body">
+      <div slot="print" let:id let:data class="card bg-secondary text-white">
+        <div class="card-body" class:done={data.done} class:text-dark={data.done}>
           <h5 class="card-title">{data.title}</h5>
           <p class="card-text">{data.description}</p>
             <button on:click={()=>document
             .querySelector("form[slot=input]")
             .querySelectorAll("input[name]")
             .forEach((a)=>{
-              a.value = data[a.name];
+              if(a.type == "checkbox") a.checked = data[a.name];else a.value = data[a.name];
               isEdit = true;
             })} class="btn btn-primary">edit</button>
             <button on:click={()=>del(id)} class="btn btn-danger">delete</button>
@@ -46,3 +53,9 @@
     </Store>
   </div>
 </Main>
+
+<style>
+  .done{
+    background-color: yellow;
+  }
+</style>
