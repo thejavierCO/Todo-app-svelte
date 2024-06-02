00ca267cb40cf4625a362856964d68012f3ecd6f
portalcruz117 <portalcruz117@gmail.com>
2024-06-02 23:30:44 +0000

    update save item

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Main from "./Main.svelte";
   import Store from "./store.svelte";
+  let isEdit = false;
 </script>
 
 <Main>
@@ -11,27 +12,35 @@
   </div>
   <div slot="content" class="col">
     <Store let:add let:edit let:del useLocalStorage>
-      <div slot="input" class="justify-content-center render row">
-        <form on:submit|preventDefault={({target:{nameItem,descriptionItem}})=>{
-            add({name:nameItem.value, description:descriptionItem.value})
-            nameItem.value = ""
-            descriptionItem.value = ""
-          }}>
-          <input type="text" placeholder="name" id="nameItem"/>
-          <input type="text" placeholder="description" id="descriptionItem"/>
+      <form slot="input" on:submit|preventDefault={({target:a})=>{
+          let data = {};
+          let inputs = a.querySelectorAll("input[name]");
+          inputs.forEach(({name,value})=>value!=""?data[name] = value:"");
+          if(isEdit) edit(data.id,data);else add(data);
+          inputs.forEach((a)=>a.value = "");
+          if(isEdit!=false)isEdit = false;
+        }} class="justify-content-center render row">
+        <input type="text" placeholder="title" name="title"/>
+        <input type="text" placeholder="description" name="description"/>
+        <input type="text" name="id" disabled/>
+        {#if isEdit}
+          <input type="submit" value="edit">
+        {:else}
           <input type="submit" value="add">
-        </form>
-      </div>
-      <div slot="print" let:id let:data let:index>
-        <div class="card bg-secondary text-white">
-          <div class="card-body">
-            <h5 class="card-title">{data.name}</h5>
-            <p class="card-text">{data.description}</p>
-              <button on:click={()=>{
-                console.warn("not working")
-              }} class="btn btn-primary">edit</button>
-              <button on:click={()=>del(id)} class="btn btn-danger">delete</button>
-          </div>
+        {/if}
+      </form>
+      <div slot="print" let:id let:data let:index class="card bg-secondary text-white">
+        <div class="card-body">
+          <h5 class="card-title">{data.title}</h5>
+          <p class="card-text">{data.description}</p>
+            <button on:click={()=>document
+            .querySelector("form[slot=input]")
+            .querySelectorAll("input[name]")
+            .forEach((a)=>{
+              a.value = data[a.name];
+              isEdit = true;
+            })} class="btn btn-primary">edit</button>
+            <button on:click={()=>del(id)} class="btn btn-danger">delete</button>
         </div>
       </div>
     </Store>
